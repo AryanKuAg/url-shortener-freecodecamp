83aa0561814c6b1e9cb580144c14e3824604cf4f
AryanKuAg <alemantrixaryan@gmail.com>
2022-05-21 11:27:53 +0530

    new way to do this

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const dns = require("dns");
 const app = express();
-var bodyParser = require("body-parser");
-const Validator = require("validator");
+const validator = require("validator");
 
 const shortUrl = {
   0: "https://www.google.com",
@@ -13,8 +12,9 @@ const shortUrl = {
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+app.use(express.urlencoded());
+app.use(express.json())
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(cors());
 
 app.use("/public", express.static(`${process.cwd()}/public`));
@@ -28,35 +28,23 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-app.post("/api/shorturl", urlencodedParser, function (req, res) {
+app.post("/api/shorturl", function (req, res) {
   let formData = req.body.url;
   formData = formData.trim();
-  // let isCorrect = Validator.isURL(formData);
-  let isCorrect =
-    formData.includes("http") &&
-    formData.includes(".") &&
-    formData.includes("/");
-  // console.log(formData.toString());
-  // dns.lookup(formData.toString(), (err, value) => {
-  //   if (!err) {
-  //     console.log(value);
-  //   } else {
-  //     console.log("error");
-  //   }
-  // });
-  // console.log("df", formData);
-  // console.log("iscorrect", isCorrect.callback());
+  // console.log(validator.isURL(formData));
 
-  // dns.lookup(formData, function (err, address, family) {
-  //   console.log("err", err);
-  //   console.log("address", address);
-  //   console.log("family", family);
-  // });
+ 
+  let isCorrect = validator.isURL(formData);
+  // let isCorrect =
+  //   formData.includes("http") &&
+  //   formData.includes(".") &&
+  //   formData.includes("/");
+ 
   if (!isCorrect) {
     res.json({ error: "invalid url" });
   } else {
     formData = formData.toLowerCase();
-    isThere = false;
+    let isThere = false;
     for (let i in shortUrl) {
       if (shortUrl[i] === formData) {
         isThere = true;
@@ -65,38 +53,18 @@ app.post("/api/shorturl", urlencodedParser, function (req, res) {
     if (!isThere) {
       // url does not exist
       shortUrl[Object.keys(shortUrl).length] = formData;
-    }
+    }}
 
-    myUrl = 0;
-    for (let i in shortUrl) {
-      if (shortUrl[i] === formData) {
-        myUrl = i;
-      }
-    }
-    res.json({ original_url: formData, shortUrl: myUrl });
-    // res.redirect("api/shorturl/?formdata=" + formData);
-    // console.log("post", req.body.url);
-  }
+  //   myUrl = 0;
+  //   for (let i in shortUrl) {
+  //     if (shortUrl[i] === formData) {
+  //       myUrl = i;
+  //     }
+  //   }
+  //   res.json({ original_url: formData, shortUrl: myUrl });
+
+ res.json({pupu:'papa'})
 });
-
-// app.get("/api/shorturl", function (req, res) {
-//   mydata = req.query.formdata;
-//   original_url = "";
-//   su = "";
-//   isFound = false;
-//   for (let i in shortUrl) {
-//     if (shortUrl[i] === mydata) {
-//       original_url = shortUrl[i];
-//       su = i;
-//       isFound = true;
-//     }
-//   }
-//   if (isFound) {
-//     res.json({ original_url: original_url, shortUrl: su });
-//   } else {
-//     res.send("Nothing found");
-//   }
-// });
 
 app.get("/api/shorturl/:index", (req, res) => {
   let index = req.params.index;
@@ -104,10 +72,7 @@ app.get("/api/shorturl/:index", (req, res) => {
   isMatch = false;
   url = "";
   for (let i in shortUrl) {
-    // console.log("key", typeof i);
-    // console.log("value", shortUrl[i]);
-    // console.log("index", typeof index);
-    // console.log("--------------------");
+    
     if (index === i) {
       // console.log("match");
       isMatch = true;
@@ -128,5 +93,3 @@ app.get("*", (req, res) => {
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
-
-// what to do
